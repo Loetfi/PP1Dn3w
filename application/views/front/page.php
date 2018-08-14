e6419a7bd7f911b2ed259b16474fbec2885a4e4c
Loetfi from Server <lutfi@mhs.ubharajaya.ac.id>
2018-08-14 14:21:28 +0000

    Migrasi dari pusdatin ke git

---
+++ application/views/front/page.php
@@ -0,0 +1,34 @@
+ 
+<!-- Page Title -->
+<div class="page-title text-center">
+    <h2 class="title"><?php echo @$content['title']; ?></h2> 
+</div> 
+<div class="container">
+
+    <div class="row about-sidebar">
+
+        <div class="col-md-9 about-content">
+
+            <section class="about-company">
+
+                <div class="com"> 
+                    <?php echo $content['content']; ?>
+                    <br><br><br><br><br><br><br>
+                </div>
+            </section> 
+
+        </div>
+
+        <div class="col-md-3 sidebar left"> 
+
+            <div class="sidebar-fact">
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+</main>
+<!-- Main Content Section -->
